0c48309989558ff716a3e457e77c3297c4406e37
Nicolas Houillon <nicolas_houillon@ens.univ-artois.fr>
2022-01-26 19:39:00 +0100

    Step 01

---
+++ src/Character.java
@@ -0,0 +1,74 @@
+public class Character {
+    private String name;
+    private int life;
+    private int agility;
+    private int strength;
+    private int wit;
+    private final String RPGClass;
+
+    public Character(String name, String RPGClass) {
+        this.name = name;
+        this.RPGClass = RPGClass;
+        this.life = 50;
+        this.agility = 2;
+        this.strength = 2;
+        this.wit = 2;
+    }
+
+    public Character(String name) {
+        this.name = name;
+        this.RPGClass = "Character";
+        this.life = 50;
+        this.agility = 2;
+        this.strength = 2;
+        this.wit = 2;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getWit() {
+        return wit;
+    }
+
+    public void setWit(int wit) {
+        this.wit = wit;
+    }
+
+    public String getRPGClass() {
+        return RPGClass;
+    }
+
+    public void attack(String arg) {
+        System.out.println(this.name+": Rrrrrrrrr....");
+    }
+}
